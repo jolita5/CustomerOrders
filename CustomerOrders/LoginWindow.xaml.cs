79d80ec23de745f8a3ca75bb0fa70c5ec660de86
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2018-12-12 23:23:08 +0200

    Login screen executed, dashboard in Main window created.

--- CustomerOrders/LoginWindow.xaml.cs
+++ CustomerOrders/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +27,42 @@ namespace CustomerOrders
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=localdb\MSSQLLocalDB; Initial Catalog=LoginUserDB; Integrated Security=True;");
 
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    String query = "SELECT COUNT(1) FROM tbUserList WHERE Username=@Username AND Password=@Password";
+                    SqlCommand sqlComd = new SqlCommand(query, sqlCon);
+                    sqlComd.CommandType = System.Data.CommandType.Text;
+                    sqlComd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                    sqlComd.Parameters.AddWithValue("@Password", txtPassword.Text);
+                    int count = Convert.ToInt32(sqlComd.ExecuteScalar());
+
+                    if(count == 1)
+                    {
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username or/and password is incorrect");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
         }
     }
 }
