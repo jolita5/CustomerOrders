8cf19fe4c798b27fa842f723a87880341c83ea29
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2018-12-14 16:31:40 +0200

    Changedt table in SQL

--- CustomerOrders/LoginWindow.xaml.cs
+++ CustomerOrders/LoginWindow.xaml.cs
@@ -34,25 +34,18 @@ namespace CustomerOrders
                 if (sqlCon.State == System.Data.ConnectionState.Closed)
                 {
                     sqlCon.Open();
-                    String query = "SELECT COUNT(1) FROM tbUserList WHERE Username=@Username AND Password=@Password";
+                    String query = "SELECT COUNT(1) FROM LoginData WHERE Username=@UserName AND Password=@Password";
                     SqlCommand sqlComd = new SqlCommand(query, sqlCon);
                     sqlComd.CommandType = System.Data.CommandType.Text;
-                    sqlComd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                    sqlComd.Parameters.AddWithValue("@UserName", txtUsername.Text);
                     sqlComd.Parameters.AddWithValue("@Password", txtPassword.Text);
                     int count = Convert.ToInt32(sqlComd.ExecuteScalar());
 
                     if(count == 1)
-                    {
-                        if(txtPassword.Text.Length >= 6)
-                        {
+                    {                    
                             MainWindow dashboard = new MainWindow();
                             dashboard.Show();
                             this.Close();
-                        }
-                       else
-                        {
-                            MessageBox.Show("Password must consists of 6 or more digits!");
-                        }
                     }
                     else
                     {
