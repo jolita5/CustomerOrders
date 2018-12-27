9977dc1e9cfbe2644f38262064efc2edc52d745c
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2018-12-28 00:35:19 +0200

    TableFromSql deleted. UsersLoginData class updated.

--- CustomerOrders/UsersLoginData.xaml.cs
+++ CustomerOrders/UsersLoginData.xaml.cs
@@ -22,29 +22,58 @@ namespace CustomerOrders
     public partial class UsersLoginData : Window
     {
 
-        TableFromSQLDataContext dc = new TableFromSQLDataContext(Properties.Settings.Default.LoginUserDBConnectionString);
-            
+
 
         public UsersLoginData()
         {
-
             InitializeComponent();
-           
-          
-            if (dc.DatabaseExists())
+
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=LoginUserDB; Integrated Security=True;");
+
+            LoginWindow login = new LoginWindow();
+            try
             {
 
-                UsersList.ItemsSource = dc.Products;
+               // sqlCon.Open();
+                //String query = "SELECT o.OrderDate, p.ProductName, p.Price_1h, p.Price_2h, l.* FROM Orders o inner join Products p on o.ProductID=p.Id inner join LoginData l on o.CustomerID=l.Id WHERE Username=@UserName AND Password=@Password";
+                //SqlCommand sqlComd = new SqlCommand(query, sqlCon);
+                //sqlComd.CommandType = System.Data.CommandType.Text;
+                //sqlComd.Parameters.AddWithValue("@UserName", login.txtUsername.Text);
+                //sqlComd.Parameters.AddWithValue("@Password", login.txtPassword.Password);
+                //sqlComd.ExecuteNonQuery();
+
+                //SqlDataAdapter dataAdp = new SqlDataAdapter(sqlComd);
+                
+                //DataTable dt = new DataTable("Info");
+                //dataAdp.Fill(dt);
+                //UsersList.ItemsSource = dt.DefaultView;
+                //dataAdp.Update(dt);
+
+
+
+                SqlCommand com = sqlCon.CreateCommand();
+                com.CommandText = "SELECT o.OrderDate, p.ProductName, p.Price_1h, p.Price_2h, l.* FROM Orders o inner join Products p on o.ProductID=p.Id inner join LoginData l on o.CustomerID=l.Id WHERE Username=@UserName AND Password=@Password";
+                com.CommandType = CommandType.Text;
+                com.Parameters.AddWithValue("@UserName", login.txtUsername.Text);
+                com.Parameters.AddWithValue("@Password", login.txtPassword.Password);
+                SqlDataReader dr = com.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                UsersList.ItemsSource = dt.DefaultView;
+                dr.Close();
+              //  sqlCon.Close();
+
 
             }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
-
-            dc.SubmitChanges();
-        }
 
         private void MakeOrderButton_Click(object sender, RoutedEventArgs e)
         {
